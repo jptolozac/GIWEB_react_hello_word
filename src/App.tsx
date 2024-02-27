4ece996bc024d9fc183bb66be441ff48198e3d8e
jptolozac <jeanpaultoloza@gmail.com>
2024-02-27 08:46:54 -0500

    primera version

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <section className='h-screen grid place-content-center '>
+      <div className="presentacion bg-slate-800 rounded-3xl h-fit p-10">
+        <img src="../public/icono.png" alt="icono de jean paul toloza" className='h-80'/>
+        <div className="text-center mt-10">
+          <h1 className='text-3xl font-bold'>Jean Paul Toloza</h1>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default App
